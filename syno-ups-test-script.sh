16ac07c01fca0e4400df5a94b0e9f441d88a1dc0
Tim Potter <musikgoat@gmail.com>
2022-03-23 03:34:31 -0700

    Initial Commit
    Initial Commit - Functional

---
+++ syno-ups-test-script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e 
+
+upscmdPy="<path to upscmd.py e.g. /volume1/homes/admin/SynoUPS-SimpleBatteryTester/upscmd.py>"
+
+if [ $# -eq 0 ]; then
+	testType="quick" # quick or deep
+elif [[ $1 == "quick" || $1 == "deep" ]]; then 
+	testType=$1
+else
+	echo "Input incorrect, exiting..."
+	exit 1
+fi
+
+echo "Starting -- $(date)"
+echo "Starting ${testType} test"
+python2 ${upscmdPy} test.battery.start.${testType}
+echo "Waiting for test results..."
+
+#Set iterations to 20 for quick test and 1440 for deep test
+if [[ ${testType} == "quick" ]]; then
+	j="20"
+else
+	j="1440"
+fi
+
+for (( i=0; i<$j; i++)); do
+	sleep 5
+	result="$(/usr/bin/upsc ups ups.test.result 2>&1 | grep -v '^Init SSL')"
+	if [[ ${result} == "Done and passed" ]]; then
+		echo "UPS Test Passed"
+		echo "Ended -- $(date)"
+		exit 0
+	elif [[ ${result} != "No test initiated" ]]; then
+		echo "--- UPS Test FAILED ---"
+		echo "Ended -- $(date)"
+		exit 1
+	fi
+done
+
+#If reached, operation failed
+echo "Operation timed out! UPS did not pass/fail in the allotted time frame"
+echo "Ended -- $(date)"
+exit 1
+
---
+++ upscmd.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python2
+# Author: Tim Potter 
+# upscmd emulator using NUT protocol  
+
+# TODO: 
+# Update to support py3 
+# DSM6 Support validation 
+# Maybe - Support outside APC Back Ups Pro
+
+# NUT protocol for reference: https://networkupstools.org/docs/developer-guide.chunked/ar01s09.html
+
+import sys
+import telnetlib
+
+user = "<upsd user>"
+pwd = "<upsd user password>"
+
+if len(sys.argv) == 2:
+    cmd = sys.argv[1]
+else:
+    print("the ups command to issue is missing.")
+    print("example: upscmd.py test.battery.start.quick")
+    exit(1)
+
+tn = telnetlib.Telnet("127.0.0.1", 3493)
+
+tn.write("USERNAME {0}\n".format(user))
+response = tn.read_until("OK", timeout=2)
+print("USERNAME: {0}".format(response.strip()))
+
+tn.write("PASSWORD {0}\n".format(pwd))
+response = tn.read_until("OK", timeout=2)
+print("PASSWORD: {0}".format(response.strip()))
+
+tn.write("INSTCMD ups {0}\n".format(cmd))
+response = tn.read_until("OK", timeout=2)
+print("INSTCMD ups {0}: {1}".format(cmd, response.strip()))
+
+if response.strip() != "OK":
+  tn.write("LIST CMD ups\n")
+  response = tn.read_until("END LIST CMD ups", timeout=2)
+  print("\n>> AVAILABLE CMDS:")
+  cmds = response.splitlines()[1:-1]
+  for cmd in cmds:
+    print(cmd.replace("CMD ups ", "- "))
+
+tn.write("LOGOUT\n")
+print tn.read_all().rstrip("\n")
